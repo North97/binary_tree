92e4dcb86c6622dd283ce7bc5609838789da8751
North97 <denis_north97@mail.ru>
2016-04-27 16:56:55 +0400

    append BinaryTree

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -3,7 +3,7 @@
  * с помощью структуры данных бинарного дерева поиска.
  *
  * */
-public class BinaryTree<T extends Comparable> {
+public class BinaryTree<T extends Comparable<T>> {
 
     /**
      * Класс, реализующий узел дерева. Реализует интерфейс Comparaable<T>
@@ -81,5 +81,80 @@ public class BinaryTree<T extends Comparable> {
         public void setParent(Node<T> parent) {
             this.parent = parent;
         }
+    }
+
+    private Node<T> root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    /**
+     * Метод, возвращающий значение true, если дерево пусто и false в противном случае.
+     * */
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void remove(T value) {
+
+    }
+
+    /**
+     * Проверка значения на вхождение в дерево.
+     *
+     * Аргументы:
+     * value -- проверяемое значение
+     * */
+    public boolean contains(T value) {
+        return findNode(root, value) != null;
+    }
+
+    /**
+     * Поиск узла в дереве с указанным значением.
+     *
+     * Аргументы:
+     * value -- искомое значение
+     * */
+    private Node<T> findNode(Node<T> node, T value) {
+        if(node != null) {
+            if (node.compareTo(value) > 0) {
+                return findNode(node.getLeft(), value);
+            } else if (node.compareTo(value) < 0) {
+                return findNode(node.getRight(), value);
+            }
+        }
+        return node;
+    }
+
+    /**
+     * Добавление вершины в дерево.
+     *
+     * Аргументы:
+     * value -- добавляемое значение
+     * */
+    public void add(T value) {
+        addHelper(root, value, null);
+    }
+
+    /**
+     * Вспомогательный рекурсивный метод для добавления верины.
+     *
+     * Аргументы:
+     * currentNode -- текущий просматриваемый узел
+     * value -- добавляемое значение
+     * parentNode -- родитель текущего просматриваемого узла
+     * */
+    private Node<T> addHelper(Node<T> currentNode, T value, Node<T> parentNode) {
+        if(currentNode == null) {
+            currentNode = new Node<>(value, parentNode);
+        }
+        else if(currentNode.compareTo(value) > 0) {
+            currentNode.setLeft(addHelper(currentNode.getLeft(), value, currentNode));
+        }
+        else if(currentNode.compareTo(value) < 0) {
+            currentNode.setRight(addHelper(currentNode.getRight(), value, currentNode));
+        }
+        return currentNode;
     }
 }
